3813158489ede4dccc5c80410390ab0a17a1cd35
sachan13harshit <harshit.23bcs10134@sst.scaler.com>
2025-01-27 00:07:00 +0530

    Deployed

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ const userRoutes = require('./routes/userRoutes');
 const gadgetsRoutes = require('./routes/gadgetsRoutes');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use('/auth', userRoutes);
 app.use('/gadgets', gadgetsRoutes);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("Server is running on port 3000");
 });
 
